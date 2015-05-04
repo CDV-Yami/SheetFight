a2218a1e12562f435bd26814c384fe196e06e70e
Kevin Gittens <kgittens973@gmail.com>
2015-05-05 00:29:13 +0200

    Add input implementation

--- spec/Yami/SheetFight/Model/InputSpec.php
+++ spec/Yami/SheetFight/Model/InputSpec.php
@@ -11,6 +11,26 @@ class InputSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
+        $this->beConstructedWith('123');
         $this->shouldHaveType('Yami\SheetFight\Model\Input');
+    }
+
+    function it_requires_a_string_to_be_constructed()
+    {
+        $this->beConstructedWith('123');
+        $this->shouldThrow('InvalidArgumentException')->during('__construct',[123]);
+    }
+
+    function it_is_an_input()
+    {
+        $this->beConstructedWith('123');
+        $this->shouldImplement('Yami\SheetFight\Model\InputInterface');
+    }
+
+    function it_has_the_input_string_representation()
+    {
+        $this->beConstructedWith('123');
+        $this->getValue()->shouldBeString();
+        $this->getValue()->shouldReturn('123');
     }
 }
--- src/Yami/SheetFight/Model/Input.php
+++ src/Yami/SheetFight/Model/Input.php
@@ -2,6 +2,39 @@
 
 namespace Yami\SheetFight\Model;
 
-class Input
+use InvalidArgumentException;
+
+/**
+ * Represents the inputs done by the player to perform a move
+ *
+ * @author Kevin GITTENS <kgittens973@gmail.com>
+ * @author Ludovic FLEURY <ludo.fleury@gmail.com>
+ */
+class Input implements InputInterface
 {
+    private $value;
+
+    /**
+     * Creates a new input
+     *
+     * @param string $value
+     */
+    public function __construct($value)
+    {
+        if (!is_string($value)) {
+            throw new InvalidArgumentException('String expected');
+        }
+
+        $this->value = $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
 }
